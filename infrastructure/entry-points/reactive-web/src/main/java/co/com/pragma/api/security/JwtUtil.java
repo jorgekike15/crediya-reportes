6352df6828dbd270de8c7cd4778480cc41b518a2
jorgekike55 <kjorge55@hotmail.com>
2025-09-03 18:07:47 -0500

    se agrega logica para que para los estados creados, se encole

---
+++ infrastructure/entry-points/reactive-web/src/main/java/co/com/pragma/api/security/JwtUtil.java
@@ -0,0 +1,31 @@
+package co.com.pragma.api.security;
+
+import io.jsonwebtoken.Jwts;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class JwtUtil {
+    private final String secretKey;
+
+    public JwtUtil(@Value("${spring.security.oauth2.resourceserver.jwt.secret}") String secretKey) {
+        this.secretKey = secretKey;
+    }
+
+    public String extractUsername(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+
+    public String extractRol(String token) {
+        return Jwts.parser()
+                .setSigningKey(secretKey)
+                .parseClaimsJws(token)
+                .getBody()
+                .get("role", String.class);
+    }
+
+}
